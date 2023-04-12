fc1f6ede6103db13e54d2c3a29aebc406d1f43c1
NatiFekadu <natifekadu808@gmail.com>
2023-04-12 13:48:57 +0300

    Hook up Sanity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React from 'react';
 // import product from '../../lingo/schemas/product'
-import { Product,FooterBanner,HeroBanner } from '@/components'
+import { Product, FooterBanner, HeroBanner } from '@/components';
+import { client } from '../../lib/client';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-    <HeroBanner/>
-     <div className='products-heading'>
-      <h2>Best Selling  Products</h2>
-      <p>Speakers of many Variations</p>
-     </div>
-     <div className='products-container'>
-      {['Product 1','Product 2'].map((product)=>product)}
-     </div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+     
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many Variations</p>
+      </div>
+      <div className="products-container">
+        {products?.map((product) => product.name)}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = '*[_type=="product"]';
+  const products = await client.fetch(query);
+  const BannerQuery = '*[_type=="banner"]';
+  const bannerData = await client.fetch(BannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+export default Home;
