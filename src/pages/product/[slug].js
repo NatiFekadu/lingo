cba7e5888148e0207e770ad93b90b7a811bd1e79
NatiFekadu <natifekadu808@gmail.com>
2023-05-18 14:54:24 +0300

    success page

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -12,7 +12,12 @@ import { Product } from '@/components';
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const {decQty,incQty,qty,onAdd} =useStateContext();
+  const {decQty,incQty,qty,onAdd,setShowCart} =useStateContext();
+
+  const handleBuyNow =()=>{
+    onAdd(product,qty);
+    setShowCart(true);
+  }
 
   return (
     <div>
@@ -59,7 +64,7 @@ const ProductDetails = ({ product, products }) => {
             <button type="button" className="add-to-cart" onClick={()=>onAdd(product,qty)}>
               Add to Cart
             </button>
-            <button type="button" className="buy-now" onClick="">
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
