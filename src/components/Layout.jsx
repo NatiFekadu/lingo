3131ae420eea2e148fa106131e1a3ffaaa186f77
NatiFekadu <natifekadu808@gmail.com>
2023-07-20 09:13:04 +0300

    removed navbar for testing

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import { Footer } from '.';
-import Navbar from './Navbar';
+//import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
   return (
@@ -9,9 +9,11 @@ const Layout = ({ children }) => {
       <Head>
         <title>Lingo</title>
       </Head>
+      {/** 
       <header>
         <Navbar as="my-navbar" />
       </header>
+      */}
       <main className="main-container">{children}</main>
       <footer>
         <Footer />
