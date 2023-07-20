5929b0081f6e43844a018e69a88b900b4de2aebd
NatiFekadu <natifekadu808@gmail.com>
2023-07-20 09:01:35 +0300

    Reseolved Navbar deployment issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import { Footer } from '.';
-import { Navbar } from './index';
+import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
   return (
